c19db9ab77cdecaacc4cc434e0afe70da7c42657
Ziad Ak <ziadakgit@gmail.com>
2021-02-05 11:38:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function getWeatherFromCity(city){
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('https://www.google.com/');
     await page.waitForXPath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
@@ -13,4 +13,25 @@ async function getWeatherFromCity(city){
     console.log('The Weather in ' + city + ' is: '+String(textV).trim()+'°C');
     browser.close();
 }
-getWeatherFromCity('Tel Aviv');
+
+async function getAmazonProducts(product,pages){
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://www.amazon.com/');
+    await page.waitForXPath('//*[@id="twotabsearchtextbox"]')
+    await page.type('input[id=twotabsearchtextbox]',product+'\n');
+    console [prices] = await page.$x('//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/div/a/span');
+    console.log(prices);
+    // await page.waitForXPath('//*[@id="wob_wc"]');
+    // const [degree] = await page.$x('//*[@id="wob_tm"]');
+    // const valueof = await degree.getProperty('textContent');
+    // const textV = await valueof.jsonValue();
+    // console.log('The Weather in ' + city + ' is: '+String(textV).trim()+'°C');
+    // browser.close();
+}
+
+//  //*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[2]/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/div/a/span
+// //*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[10]/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/div/a/span
+
+//getWeatherFromCity('Haifa');
+getAmazonProducts('iphone',2);
