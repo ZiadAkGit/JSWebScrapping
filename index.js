466eb1d8234cac993255591a0daae990736ad4b9
Ziad Ak <ziadakgit@gmail.com>
2021-02-05 12:39:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ async function getAmazonProducts(product,pages){
     await page.type('input[id=twotabsearchtextbox]',product+'\n');
     await page.waitForXPath('//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[24]/span/div/div/ul/li[3]/a');
     const [prices] = await page.$x('//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[24]/span/div/div/ul/li[3]/a');
-    
     await page.click('#search > div.s-desktop-width-max.s-desktop-content.sg-row > div.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span:nth-child(4) > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(24) > span > div > div > ul > li:nth-child(3) > a');
-    
     // await page.waitForXPath('//*[@id="wob_wc"]');
     // const [degree] = await page.$x('//*[@id="wob_tm"]');
     // const valueof = await degree.getProperty('textContent');
